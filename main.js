5b34a4ef18ef6ef105927dd5d8d867505769054f
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-04-07 19:55:55 -0700

    Finished Site

--- main.js
+++ main.js
@@ -4,11 +4,20 @@ const primaryNav = document.querySelector('#primary-nav');
 navToggle.addEventListener('click',() => {
     const navOpened = navToggle.getAttribute('aria-expanded');
     if(navOpened === 'false'){
-         navToggle.setAttribute("aria-expanded","True");
+         navToggle.setAttribute("aria-expanded","true");
     } else{
-         navToggle.setAttribute("aria-expanded", "False");
+         navToggle.setAttribute("aria-expanded", "false");
     }
        
 
     console.log(navOpened);
 });
+const resizeObserver = new ResizeObserver (() => {
+     document.body.classList.add("resizing");
+ 
+     requestAnimationFrame(() => {
+          document.body.classList.remove("resizing")
+     });
+});
+
+resizeObserver.observe(document.body);
